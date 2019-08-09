39f56a396abede4e0fd6724abc230206f81f86d9
devjaenqn <43260625+devjaenqn@users.noreply.github.com>
2019-08-09 00:31:54 -0500

    Update 2015_12_04_003040_add_special_role_column.php

--- migrations/2015_12_04_003040_add_special_role_column.php
+++ migrations/2015_12_04_003040_add_special_role_column.php
@@ -11,11 +11,13 @@ class AddSpecialRoleColumn extends Migration
      */
     public function up()
     {
+        /*
         $name = config('shinobi.tables.roles');
 
         Schema::table($name, function ($table) {
             $table->enum('special', ['all-access', 'no-access'])->nullable();
         });
+        */
     }
 
     /**
@@ -25,10 +27,12 @@ class AddSpecialRoleColumn extends Migration
      */
     public function down()
     {
+        /*
         $name = config('shinobi.tables.roles');
 
         Schema::table($name, function ($table) {
             $table->dropColumn('special');
         });
+        */
     }
 }
